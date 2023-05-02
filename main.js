13c10737e92f42e27d1ee999d59adebcce2643a9
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-02 18:29:41 -0500

    feat: :tada: Inicio de proyecto sobre el consumo de la API de la nasa

---
+++ main.js
@@ -0,0 +1,4 @@
+//console.log("https://api.nasa.gov/planetary/apod?api_key=f3Z7FJLTQgj2vWXyWVki2KaeGURpf26RmZGdm5W6&start_date=2021-01-01&end_date=2023-01-01&thumbs");
+import results from "./components/consulta_ETC.js";
+
+console.log(results.dataconsulta());
