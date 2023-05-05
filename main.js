c86eddf63578c2865c7cab43ba3f7a2703787921
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-05 09:23:10 -0500

    feat: :sparkles: arreglo en el archivo app

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 //console.log("https://api.nasa.gov/planetary/apod?api_key=f3Z7FJLTQgj2vWXyWVki2KaeGURpf26RmZGdm5W6&start_date=2021-01-01&end_date=2023-01-01&thumbs");
-
-
 import mostrarDatos from './components/mostrarDatos.js';
-
 mostrarDatos.datos();
+
+
 
 /* Extraemos la fecha actual */
 const input = document.querySelector("input");
