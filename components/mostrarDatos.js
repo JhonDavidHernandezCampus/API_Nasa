bcd42013bd4889a4a161c6389ab2a35bbfedccd8
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-04 23:15:53 -0500

    feat: :sparkles: se termilo pa parte de pintar los datos

--- components/mostrarDatos.js
+++ components/mostrarDatos.js
@@ -2,13 +2,14 @@ import storage from '../consulta_API/consulta_ETC.js';
 export default{
     async datos(){
         const data = await storage.dataconsulta();
-        console.log(data);
         const elements = document.querySelector('.elements');
         const ws = new Worker("./storage/ws.js",{type:"module"});
         ws.postMessage({module: "pintarData",data: data});
 
         ws.addEventListener("message",(e)=>{
-            console.log(e.data);
+            let section = document.querySelector(".elements");
+            section.innerHTML = e.data;
+            section.style.backgroundImage= `url(${data.url})`;
         })
     }
 
--- storage/ws.js
+++ storage/ws.js
@@ -1,10 +1,20 @@
 
 let ws = {
     pintarData(data){
-        return data
+        let vard = "";
+        vard =`
+    <div class="informacion">
+        <h1>Informacion</h1>
+        <h5>${data.title}</h5>
+        <p>Fecha: ${data.date}</p>
+        <p> Explicacion: ${data.explanation}</p>
+        <p>Autor: ${data.copyright}</p>
+        <a href="${data.url}" class="btn btn-info" target="_blank">Ver imagen</a>
+    </div>`;
+        return vard;
+
     }
 }
 self.addEventListener("message",(e)=>{
     postMessage(ws[`${e.data.module}`](e.data.data))
-    console.log(e.data.module);
 })
